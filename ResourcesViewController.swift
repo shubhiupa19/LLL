4fcd0965e0eeb8a32f783fcae7088fd7e5ec207a
Shubhi Upadhyay <s.x.upadhyay@gmail.com>
2022-08-03 10:20:59 -0400

    finished resources view controller

--- ResourcesViewController.swift
+++ ResourcesViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
-
+import WebKit
 class ResourcesViewController: UIViewController {
-
+    @IBOutlet weak var wv: WKWebView!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadYoutube(videoID: "oCm_lnoVf08")
 
         // Do any additional setup after loading the view.
     }
@@ -31,10 +34,34 @@ class ResourcesViewController: UIViewController {
         openUrl(urlStr: "https://www.epa.gov/facts-and-figures-about-materials-waste-and-recycling/national-overview-facts-and-figures-materials#recycling")
     }
     
+    
+    @IBAction func COVID19ButtonTapped(_ sender: Any) {
+        openUrl(urlStr: "https://www.countryliving.com/uk/news/a40767741/covid-19-litter-environment/")
+    }
+    
+    @IBAction func plasticButtonTapped(_ sender: Any) {
+        openUrl(urlStr: "https://education.nationalgeographic.org/resource/whopping-91-percent-plastic-isnt-recycled")
+    }
+    
+    @IBAction func currentButtonTapped(_ sender: Any) {
+        openUrl(urlStr: "https://www.nycfoodpolicy.org/food-policy-snapshot-california-leglislation-to-phase-out-single-use-plastics/")
+    }
+    
+    
     func openUrl(urlStr: String!) {
         if let url = URL(string:urlStr), !url.absoluteString.isEmpty {
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
+    func loadYoutube(videoID videoID:String) {
+            // create a custom youtubeURL with the video ID
+            guard
+                let youtubeURL = NSURL(string: "https://www.youtube.com/embed/HQTUWK7CM-Y")
+                else { return }
+            // load your web request
+        wv.load( NSURLRequest(url: youtubeURL as URL) as URLRequest )
+        }
+    
+    
     
 }
